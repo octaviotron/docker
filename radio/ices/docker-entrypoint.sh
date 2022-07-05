ca0caf34a4477daeb7511803538ec1f759cea125
Octavio Rossell <octavio.rossell@gmail.com>
2022-07-04 21:37:55 -0400

    First try

---
+++ radio/ices/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ -n "$ICES_MOUNTPOINT" ]; then
+	sed -i "s/x/x/g" /etc/ices/ices.conf
+fi
+
+find /mnt/music/* -iname "*.mp3" > /etc/ices/playlist.txt
+/usr/local/bin/ices -c /etc/ices/ices.conf -v
+
