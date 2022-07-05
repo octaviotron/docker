bf4d8fa1b6fa43f75c9fdd01d6e90326ed24f168
Octavio Rossell <octavio.rossell@gmail.com>
2022-07-05 00:39:18 -0400

    Updated env vars shift

--- radio/liquidsoap/docker-entrypoint.sh
+++ radio/liquidsoap/docker-entrypoint.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 
-if [ -n "$LIQUIDSOAP_USER" ]; then
-    sed -i "s/user.*/user='$LIQUIDSOAP_USER',/g" /etc/liquidsoap.liq
-fi
-
-if [ -n "$LIQUIDSOAP_PASS" ]; then
-    sed -i "s/password.*/password='$LIQUIDSOAP_PASS',/g" /etc/liquidsoap.liq
-fi
-
-if [ -n "$LIQUIDSOAP_HOST" ]; then
-    sed -i "s/host.*/host='$LIQUIDSOAP_HOST',/g" /etc/liquidsoap.liq
-fi
-
-if [ -n "$LIQUIDSOAP_MOUNT" ]; then
-    sed -i "s/mount.*/mount='$LIQUIDSOAP_MOUNT',/g" /etc/liquidsoap.liq
-fi
+echo '#!/usr/bin/liquidsoap' > /etc/liquidsoap.liq
+echo 'security = mksafe(single("/var/liquidsoap/music/default.ogg"))' >> /etc/liquidsoap.liq
+echo "ices = input.http('http://$LIQUIDSOAP_HOST:8000/ices')" >> /etc/liquidsoap.liq
+echo "envivo = input.http('http://$LIQUIDSOAP_HOST:8000/envivo')" >> /etc/liquidsoap.liq
+echo "" >> /etc/liquidsoap.liq
+echo "verif = fallback([envivo, ices])" >> /etc/liquidsoap.liq
+echo "radio = fallback([verif, security])" >> /etc/liquidsoap.liq
+echo "" >> /etc/liquidsoap.liq
+echo "output.icecast(%vorbis, host='$LIQUIDSOAP_HOST', port = 8000, password='$LIQUIDSOAP_PASS', mount = '$LIQUIDSOAP_MOUNT', radio)" >> /etc/liquidsoap.liq
 
 runuser -l eldj -c 'liquidsoap /etc/liquidsoap.liq'
 
