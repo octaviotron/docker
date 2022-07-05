29a006193826942b2a068518970e28f5578aea6a
Octavio Rossell <octavio.rossell@gmail.com>
2022-07-04 23:27:01 -0400

    Update entrypoint config

--- radio/ices/docker-entrypoint.sh
+++ radio/ices/docker-entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-if [ -n "$ICES_MOUNTPOINT" ]; then
-	sed -i "s/x/x/g" /etc/ices/ices.conf
+if [ -n "$ICECAST_HOSTNAME" ]; then
+	sed -i "s/<Hostname>[^<]*<\/Hostname>/<Hostname>$ICECAST_HOSTNAME<\/Hostname>/g" /etc/ices/ices.conf
 fi
 
-find /mnt/music/* -iname "*.mp3" > /playlist.txt
+if [ -n "$ICECAST_PASSWORD" ]; then
+    sed -i "s/<Password>[^<]*<\/Password>/<Password>$ICECAST_PASSWORD<\/Password>/g" /etc/ices/ices.conf
+fi
+
+find /mnt/music/* -iname "*.mp3" > playlist.txt
 /usr/local/bin/ices -c /etc/ices/ices.conf -v
 
